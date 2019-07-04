d83b2bf1dbadca0140cd390e5a8f108022fcfda8
Belal <belaltarek96@gmail.com>
2019-07-04 15:37:28 +0200

    initialization2

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
-    uiPort: process.env.PORT || 1880,
+    uiPort: process.env.PORT || 3002,
 
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // To listen on all IPv6 addresses, set uiHost to "::",
@@ -269,7 +269,7 @@ module.exports = {
     editorTheme: {
         projects: {
             // To enable the Projects feature, set this value to true
-            enabled: true
+            enabled: false
         }
     }
 }
